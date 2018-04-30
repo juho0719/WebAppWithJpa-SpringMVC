9003cc66811fdb7a755838f946c223acacc937c2
Juho Kim <juho0719@naver.com>
2018-05-01 01:54:18 +0900

    update

--- src/main/java/com/juho/springmvctest/model/UserRepository.java
+++ src/main/java/com/juho/springmvctest/model/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<MyUser, Integer> {
     MyUser findByUsername(String userName);
+    
 }
